5823a8347230bc7e8dd9a90df82d5d72639c1e5f
Cesar Augusto Gonçalves De Lima <cglima@VRB-MB-19432.bbrouter>
2024-03-13 20:54:11 -0300

    feat: conversao de temperatura

--- app/src/main/java/com/example/androidclasses/MainActivity.kt
+++ app/src/main/java/com/example/androidclasses/MainActivity.kt
@@ -11,14 +11,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+//Conversor euro
+//        binding.buttonSubmit.setOnClickListener{
+//            var euro = 5.5
+//            var inuptValue = binding.inputTextIn.text.toString().replace(",",".").toDouble()
+//            var result = String.format("%.2f", euro * inuptValue).toDouble()
+//            var result2 = (euro * inuptValue * 100).roundToInt().toDouble() / 100
+//            binding.textViewOutput.setText("R$: ${result}\nR$: ${result2}");
+//        }
 
-        binding.buttonSubmit.setOnClickListener{
-            var euro = 5.5
-            var inuptValue = binding.inputTextIn.text.toString().replace(",",".").toDouble()
-            var result = String.format("%.2f", euro * inuptValue).toDouble()
-            var result2 = (euro * inuptValue * 100).roundToInt().toDouble() / 100
-            binding.textViewOutput.setText("R$: ${result}\nR$: ${result2}");
+//        Conversor Temperatura
+        binding.buttonSubmit.setOnClickListener {
+            if(!binding.inputTextIn.text.toString().isEmpty()) {
+                var celsius = binding.inputTextIn.text.toString().toDouble()
+                var fahrenheit = String.format("%.2f",  celsius * 1.8 + 32)
+
+                binding.textViewOutput.text = fahrenheit + " F"
+            }else{
+                binding.textViewOutput.text = "Temperatura inválida"
+            }
+
+
         }
-
     }
 }
